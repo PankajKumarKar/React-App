d854bb5afe154fbcb65e164b399bb559d0422425
PankajKumarKar <pankajkar14@gmail.com>
2023-07-25 14:28:05 +0530

    first commit

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,52 @@
+import React from "react";
+import classes from "./header.module.css";
+import { Link } from "react-router-dom";
+
+function Header() {
+  const user = {
+    name: "Pankaj",
+  };
+
+  const cart = {
+    totalcount: 10,
+  };
+
+  const logout = () => {};
+
+  return (
+    <header className={classes.header}>
+      <div className={classes.container}>
+        <Link to="/" className={classes.logo}>
+          Foodie!
+        </Link>
+        <nav>
+          <ul>
+            {user ? (
+              <li className={classes.menu_container}>
+                <Link to="/profile">{user.name}</Link>
+                <div className={classes.menu}>
+                  <Link to="/profile">Profile</Link>
+                  <Link to="/orders">Orders</Link>
+                  <a onClick={logout}>Logout</a>
+                </div>
+              </li>
+            ) : (
+              <Link to="/login"></Link>
+            )}
+
+            <li>
+              <Link to="/cart">
+                Cart
+                {cart.totalcount > 0 && (
+                  <span className={classes.cart_count}>{cart.totalcount}</span>
+                )}
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
